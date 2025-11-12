5cd4978244b3263505fe6c8eb15f70dde901af69
Sascha Bergenås <sascha.bergenas@hotmail.com>
2025-11-12 22:28:50 +0100

    imported setWeatherBackground, changed position to sundsvall

--- main.js
+++ main.js
@@ -2,12 +2,7 @@ import { handleSearch, updateList } from "./input.js";
 import { Weather } from "./mainWeather.js";
 import { Searched } from "./searchHistory.js";
 import { getCity, getWeather } from "./services.js";
-
-const searchField = document.getElementById("search-input");
-
-const locationContainer = document.querySelector(".location-container");
-const temperatureContainer = document.querySelector(".temperature-container");
-const forecastContainer = document.querySelector(".forecast-container");
+import { setWeatherBackground } from "./weatherBackgrounds.js";
 
 const cityList = document.querySelector("#cities");
 const inputField = document.querySelector("#search-input");
@@ -22,6 +17,7 @@ cityList.addEventListener("click", async (e) => {
   inputField.value = "";
 
   const data = await getWeather(li.dataset.lat, li.dataset.lon);
+  setWeatherBackground(data);
 
   const weatherItem = document.querySelector(".weather");
   if (weatherItem) weatherItem.remove();
@@ -44,44 +40,18 @@ cityList.addEventListener("click", async (e) => {
 
   console.log(data);
 });
-const cityName = document.createElement("h2");
-cityName.textContent = result.location.name;
-
-const cityRegion = document.createElement("h3");
-cityRegion.textContent = result.location.region;
-
-const cityCountry = document.createElement("p");
-cityCountry.textContent = result.location.country;
-
-locationContainer.appendChild(cityName);
-locationContainer.appendChild(cityRegion);
-locationContainer.appendChild(cityCountry);
-
-const currentTemp = document.createElement("p");
-currentTemp.classList.add("current-temp");
-currentTemp.innerHTML =
-  result.current.temp_c + "<span class='degree'>°C</span>";
-
-const currentCondition = document.createElement("p");
-currentCondition.textContent = result.current.condition.text;
-
-temperatureContainer.appendChild(currentTemp);
-temperatureContainer.appendChild(currentCondition);
-
-/* Dynamic background */
-const currentConditionCode = result.current.condition.code;
-let background = backgrounds[currentConditionCode];
-
-document.body.style.backgroundImage = "url('images/" + background + "')";
 
 navigator.geolocation.getCurrentPosition(async (pos) => {
   const lat = pos.coords.latitude;
   const lon = pos.coords.longitude;
+  console.log(pos);
 
   const data = await getWeather(lat, lon);
   const weatherItem = document.querySelector(".weather");
 
+  setWeatherBackground(data);
+
   console.log(lat, lon, data);
 
-  new Weather("Position", "", lat, lon, data);
+  new Weather("Sundsvall", "Sverige", lat, lon, data);
 });
